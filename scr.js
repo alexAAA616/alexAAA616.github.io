0c847e2c3841eceee07e9a88b4a880a7671d679b
alexAAA616 <68006318+alexAAA616@users.noreply.github.com>
2020-07-08 12:58:57 +0300

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,23 @@
+var canvas = document.getElementById("canvas"), 
+                context = canvas.getContext("2d");
+                 
+            var img = new Image();
+            img.src = "dubi.png";
+            img.onload = function() {
+                 
+                context.drawImage(img, 0, 0);
+                var imageData = context.getImageData(0,0, img.width, img.height);
+                var red, green, blue, greyscale;
+                 
+                for (var i = 0; i < imageData.data.length; i += 4) {
+                    red = imageData.data[i]; // получаем компоненту красного цвета
+                    green = imageData.data[i + 1];  // получаем компоненту зеленого цвета
+                    blue = imageData.data[i + 2];   // получаем компоненту синего цвета
+                    grayscale = red * 0.3 + green * 0.59 + blue * 0.11; // получаем серый фон
+                    imageData.data[i] = grayscale;  // установка серого цвета
+                    imageData.data[i + 1] = grayscale;
+                    imageData.data[i + 2] = grayscale;
+                }
+                 
+                context.putImageData(imageData, img.width + 10, 0);
+            };
