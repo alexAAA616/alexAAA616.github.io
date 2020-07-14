3ae82133f03b8aa089ea4ead18a4e111bf3a2c58
alexAAA616 <68006318+alexAAA616@users.noreply.github.com>
2020-07-14 03:12:12 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("canvas"),
                 context = canvas.getContext("2d");
                  
             var img = new Image();
-            img.src = "https://st.depositphotos.com/1757583/2169/i/450/depositphotos_21696885-stock-photo-yin-yang-symbol.jpg";
+            /*img.src = "https://st.depositphotos.com/1757583/2169/i/450/depositphotos_21696885-stock-photo-yin-yang-symbol.jpg";
             img.onload = function() {
                  
                 context.drawImage(img, 0, 0);
@@ -20,4 +20,9 @@ var canvas = document.getElementById("canvas"),
                 }
                  
                 context.putImageData(imageData, img.width + 10, 0);
-            };
+            };*/
+
+
+let input = document.getElementById('input');
+
+input.onchange = () => { img.src =  window.URL.createObjectURL(input.files[0]); img.onload = () => {context.drawImage(img, 0, 0); alert(context.getImageData(0,0, img.width, img.height).data[0]); }}
